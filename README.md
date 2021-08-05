1f75c77010658b7a8b2400a545fa85dc7e7f337e
aseani miller <milleraseani@gmail.com>
2021-08-05 08:25:58 -0700

    Version 1.0.2

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 >I originally built this game on my phone as a beginner python programmer but I looked back on that source code with shame, 
 >as it took up atleast 602 lines of code on my phone (roughly 380-400 on a pc) I had to make a much sophisticated version,
 >so here we are. I just need to create an algorithm to determine the winner and im finished with this project
+## Version 1.0.2
+>Created the necessary computer algorithm to solve the connect four puzzle (almost finished), when it comes to figuring out who won the game
+>going to need to figure out how to detect the winner diagonally, which is the true test for sure.
--- c4.py
+++ c4.py
@@ -3,7 +3,6 @@ import tkinter.font as font
 
 window    = Tk()
 
-
 slot      =  "O"
 player_1  =  "❅"
 player_2  =  "✪"
@@ -24,10 +23,68 @@ p_turn         = Label(window,text="Player 1 Goes First!!!")
 p_turn['font'] = font.Font(size=13)
 p_turn.grid(row=0,column=1)
 preview()
+
+def define_winner():
+  for row in board:
+      counter   = 0
+      i         = 0
+      for element in row:
+        if element != slot:
+          counter   = 0
+          for el in row[i:-1:]:
+            if el == player_1:
+              counter+=1
+            elif el == player_2:
+              counter-=1
+          if counter >= 4 or counter <= -4:
+            print(counter)
+            return counter 
+          else:
+            counter   = 0         
+          i+=1  
+  chip_num   = 0
+  
+  for row in range(7): 
+   counter_v  = 0
+   i=-1
+   for _ in range(len(board)): 
+    for chip in board[i][chip_num]:
+      
+      if chip != slot:
+       z=i
+       counter_v  = 0
+       
+       for _ in range(abs((len(board) - len(board)*2) + abs(z)) + 1):
+        
+        for piece in board[z][chip_num]: 
+         
+         
+         if piece == player_1:
+           counter_v+=1
+           
+        
+         elif piece == player_2:
+           counter_v-=1
+           print(counter_v)
+         z-=1  
+      
+        if counter_v >= 4 or counter_v <= -4:
+           print("vertical win" ,counter_v)
+           return counter_v      
+    i-=1
+    
+    
+   chip_num+=1
+
+
+
+
+
+
 def ply(t,  choose,tur):
   global turn
   turn = t
-  print(choose)
+
 
   whose_turn  = "It is Player 1's turn" if turn==False else "It is Player 2's turn"
   
@@ -43,29 +100,52 @@ def ply(t,  choose,tur):
             if board[i][choice]==slot:
                 board[i][choice]= player_1 if turn==False else player_2
                 game=False
-                turn = not turn
-                print(turn) 
+                turn = not turn 
  
             i-=1
           except:
-              print(choose)
               i=-1
               game=False
   whose_turn  = "It is Player 1's turn" if turn==False else "It is Player 2's turn"
   global p_turn
   p_turn.config(text=whose_turn)
+  if define_winner() == 4:
+    p_turn.config(text="Player 1 has won the game")
+    end_game()
+  elif define_winner() == -4:
+    end_game()
+    p_turn.config(text='Player 2 has won the game')  
   
   preview()
       
 
 
-slot_0   =  Button(window, text=f"slot - 0",command=lambda: ply(turn, 0,p_turn)).grid(row=1,column=2)
-slot_1   =  Button(window,text=f"slot 1",command=lambda: ply(turn,1,p_turn)).grid(row=1,column=3)
-slot_2   =  Button(window,text=f"slot 2",command=lambda: ply(turn,2,p_turn)).grid(row=1,column=4)
-slot_3   =  Button(window,text=f"slot 3",command=lambda: ply(turn,3,p_turn)).grid(row=2,column=2)
-slot_4   =  Button(window,text=f"slot 4",command=lambda: ply(turn,4,p_turn)).grid(row=2,column=3)
-slot_5   =  Button(window,text=f"slot 5",command=lambda: ply(turn,5,p_turn)).grid(row=2,column=4)
-slot_6   =  Button(window,text=f"slot 6",command=lambda: ply(turn,6,p_turn)).grid(row=3,column=2)
+slot_0   =  Button(window, text=f"slot - 0",command=lambda: ply(turn, 0,p_turn))
+slot_1   =  Button(window,text=f"slot 1",command=lambda: ply(turn,1,p_turn))
+slot_2   =  Button(window,text=f"slot 2",command=lambda: ply(turn,2,p_turn))
+slot_3   =  Button(window,text=f"slot 3",command=lambda: ply(turn,3,p_turn))
+slot_4   =  Button(window,text=f"slot 4",command=lambda: ply(turn,4,p_turn))
+slot_5   =  Button(window,text=f"slot 5",command=lambda: ply(turn,5,p_turn))
+slot_6   =  Button(window,text=f"slot 6",command=lambda: ply(turn,6,p_turn))
+
+slot_0.grid(row=1,column=2)
+slot_1.grid(row=1,column=3)
+slot_2.grid(row=1,column=4)
+slot_3.grid(row=2,column=2)
+slot_4.grid(row=2,column=3)
+slot_5.grid(row=2,column=4)
+slot_6.grid(row=3,column=2)
+
+
+def end_game():
+  slot_0.destroy()
+  slot_1.destroy()
+  slot_2.destroy()
+  slot_3.destroy()
+  slot_4.destroy()
+  slot_5.destroy()
+  slot_6.destroy()
+
 
 
 
